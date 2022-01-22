dbd26bed19ba8c47346b2b16d856756a10d7f455
Yelk11 <neighbour.matthew@gmail.com>
2022-01-22 18:30:00 -0500

    Update App.js

--- ScholarshipFinder/App.js
+++ ScholarshipFinder/App.js
@@ -58,15 +58,5 @@ function DetailsScreen({ navigation }) {
   );
 }
 
- function App() {
-   return (
-     <NavigationContainer>
-       <Stack.Navigator initialRouteName="Home">
-         <Stack.Screen name="Home" component={HomeScreen} />
-         <Stack.Screen name="Details" component={DetailsScreen} />
-       </Stack.Navigator>
-     </NavigationContainer>
-   );
- }
 
 export default App;
