edf37c9714edf04ebd27ec9bb446fdd7f3611023
bg2020 <60444963+bg2020@users.noreply.github.com>
2022-03-16 16:52:43 -0400

    adding venv folder
    adds the venv folder to the DataEntry folder

--- DataEntry/ScholarshipDB.py
+++ DataEntry/ScholarshipDB.py
@@ -1,7 +1,6 @@
 
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("DataEntry/scholarshipfinder-fb492-firebase-adminsdk-illqy-a6c643e370.json")
 firebase_admin.initialize_app(cred)
@@ -10,10 +9,10 @@ db = firestore.client()
 
 
 data = {
-    u'name': u'Los Angeles',
-    u'state': u'CA',
+    u'name': u'TEST SCHOLARSHIP',
+    u'state': u'MI',
     u'country': u'USA'
 }
 
 # Add a new doc in collection 'cities' with ID 'LA'
-db.collection(u'cities').document(u'LA').set(data)
+db.collection(u'scholarships').document(u'DocumentName').set(data)
