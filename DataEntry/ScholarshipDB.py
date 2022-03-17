ad36f407498cc2a23f37d4c30694aa2510932d86
bg2020 <60444963+bg2020@users.noreply.github.com>
2022-03-17 11:33:43 -0400

    Update ScholarshipDB.py
    Added comments for data types

--- DataEntry/ScholarshipDB.py
+++ DataEntry/ScholarshipDB.py
@@ -9,10 +9,17 @@ db = firestore.client()
 
 
 data = {
-    u'name': u'TEST SCHOLARSHIP',
+    u'title': u'TEST SCHOLARSHIP 2',
     u'state': u'MI',
-    u'country': u'USA'
+    u'country': u'USA',
+    u'amount': 6400,
+    u'Array': [1,2,3,4,5],
+    u'boolean': 1<0,
+    u'country type': None
 }
 
+# Array needs [] and commas to work
+# Boolean needs 1 AND 0, 1 is true and 0 is false
+# Timestamp isn't able to work
 # Add a new doc in collection 'cities' with ID 'LA'
-db.collection(u'scholarships').document(u'DocumentName').set(data)
+db.collection(u'scholarships').document(U'TEST 2').set(data)
