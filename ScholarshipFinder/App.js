e413d94b538355a2ad9b607606585690ff034721
Renyah-A <abdulmaseeh2@oakland.edu>
2022-04-21 00:23:08 -0400

    set back BrowseScholarship

--- ScholarshipFinder/App.js
+++ ScholarshipFinder/App.js
@@ -46,7 +46,7 @@ const App = () => {
 
         <Stack.Navigator
 
-          initialRouteName="WelcomeScreen"
+          initialRouteName="BrowseScholarships"
           screenOptions={{
 
             headerRight: () => (
